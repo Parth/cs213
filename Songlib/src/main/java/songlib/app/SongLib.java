d50123dd98e483fb646d914d93cce2faff6b2e3d
shon615 <shon615@gmail.com>
2017-09-25 14:39:47 -0400

    added sample code

--- Songlib/src/main/java/songlib/app/SongLib.java
+++ Songlib/src/main/java/songlib/app/SongLib.java
@@ -3,32 +3,37 @@ package songlib.app;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
  
 public class SongLib extends Application {
-    public static void main(String[] args) {
-        launch(args);
-    }
-    
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
-    }
+	
+	@Override
+	public void start(Stage primaryStage) 
+	throws Exception {
+		FXMLLoader loader = new FXMLLoader();   
+	      loader.setLocation(
+	         getClass().getResource("/view/List.fxml"));
+	      AnchorPane root = (AnchorPane)loader.load();
+
+	      SongLibController listController = 
+	         loader.getController();
+	      listController.start(primaryStage);
+
+	      Scene scene = new Scene(root, 200, 300);
+	      primaryStage.setScene(scene);
+	      primaryStage.show(); 
+
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+
 }
